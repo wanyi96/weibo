85275553e7fcc47fd4ae35e673d47cfce55eee00
wanyi <408629610@qq.com>
2020-11-20 04:17:05 +0000

    11-21 展示用户微博列表

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -36,9 +36,10 @@ class UserController extends Controller
 
     public function show(User $user)
     {
+        $statuses = $user->statuses()->orderby('created_at','desc')->paginate(10);
         //将用户对象 $user 通过 compact 方法转化为一个关联数组，
         //并作为第二个参数传递给 view 方法，将数据与视图进行绑定。
-        return view('users.show',compact('user'));
+        return view('users.show',compact('user','statuses'));
     }
 
     public function store(Request $request)
