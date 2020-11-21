5335d9477edb64151ad0ba498a77e59507450e45
wanyi <408629610@qq.com>
2020-11-21 13:19:05 +0000

    11-21 社交统计信息

--- app/Models/User.php
+++ app/Models/User.php
@@ -66,7 +66,7 @@ class User extends Authenticatable
     public function follow($user_ids)
     {
         if(!is_array($user_ids)){
-            $user_ids = compact(user_ids);
+            $user_ids = compact('user_ids');
         }
         $this->followings()->sync($user_ids,false);
     }
